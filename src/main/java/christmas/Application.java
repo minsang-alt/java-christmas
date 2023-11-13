1a35832547440180f061bcc6b99b6b55726fb5ee
minsang-alt <mazinger97@naver.com>
2023-11-13 21:28:40 +0900

    feat: controller구현(예외처리 제외)

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,28 @@
 package christmas;
 
+import christmas.event.ChristmasDdayDiscount;
+import christmas.event.Event;
+import christmas.event.GiftEvent;
+import christmas.event.SpecialDiscount;
+import christmas.event.WeekdayDiscount;
+import christmas.event.WeekendDiscount;
+import christmas.service.ChristmasService;
+import christmas.service.EventService;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        List<Event> events = List.of(new ChristmasDdayDiscount(), new WeekdayDiscount(), new WeekendDiscount(),
+                new SpecialDiscount(), new GiftEvent());
+        EventService eventService = new EventService(events);
+        ChristmasService christmasService = new ChristmasService(eventService);
+
+        ChristmasController controller = new ChristmasController(inputView, outputView, christmasService);
+        controller.run();
+
     }
 }
---
+++ src/main/java/christmas/ChristmasController.java
@@ -0,0 +1,46 @@
+package christmas;
+
+import christmas.dto.EventResultDto;
+import christmas.dto.OrderDto;
+import christmas.service.ChristmasService;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class ChristmasController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final ChristmasService christmasService;
+
+    public ChristmasController(InputView inputView, OutputView outputView, ChristmasService christmasService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.christmasService = christmasService;
+    }
+
+    public void run() {
+        outputView.printWelcome();
+        OrderDto orderDto = receiveOrder();
+        beforeEventAppliedOrder(orderDto);
+        AfterEventAppliedOrder(orderDto.getEventResultDto());
+    }
+
+    private void AfterEventAppliedOrder(EventResultDto eventResultDto) {
+        outputView.printGiftMenu(eventResultDto.getGift());
+        outputView.printBenefitsHistory(eventResultDto.getBenefitDetails());
+        outputView.printTotalBenefitOrderAmount(eventResultDto.getTotalBenefitsAmount());
+        outputView.printEstimatedAmountAfterDiscount(eventResultDto.getSalesPriceAfterDiscount());
+        outputView.printEventBadge(eventResultDto.getEventBadge());
+    }
+
+    private void beforeEventAppliedOrder(OrderDto orderDto) {
+        outputView.printEventPreview(orderDto.getDate());
+        outputView.printOrderMenuList(orderDto.getOrderMenuList());
+        outputView.printTotalOrderAmount(orderDto.getTotalAmount());
+    }
+
+    private OrderDto receiveOrder() {
+        int date = inputView.readDate();
+        String inputMenuWithCount = inputView.inputMenuWithCount();
+        return christmasService.createOrderAndApplyEvent(date, inputMenuWithCount);
+    }
+}
